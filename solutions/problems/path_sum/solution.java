458af6c99678629d31bca10a19d55a587ff68298
Vaibhavi Singh <contactvaibhavisingh@gmail.com>
2023-05-24 11:49:24 +0000

    Sync LeetCode submission - Path Sum (java)

---
+++ solutions/problems/path_sum/solution.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isLeaf(TreeNode root)
+    {
+        if (root == null)
+        return false;
+
+        return (root.left == null && root.right == null);
+    }
+
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null)
+        return false;
+
+        if (isLeaf(root))
+        return root.val == targetSum;
+        
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+    }
+}
