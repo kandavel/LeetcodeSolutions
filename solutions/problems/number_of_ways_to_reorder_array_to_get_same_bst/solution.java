0a5ab2f59959955eaa0b5b143d0b8096f2d61258
Vaibhavi Singh <contactvaibhavisingh@gmail.com>
2023-06-25 04:09:53 +0000

    Sync LeetCode submission - Number of Ways to Reorder Array to Get Same BST (java)

--- solutions/problems/number_of_ways_to_reorder_array_to_get_same_bst/solution.java
+++ solutions/problems/number_of_ways_to_reorder_array_to_get_same_bst/solution.java
@@ -1,43 +1,69 @@
 class Solution {
-    private static final int MOD = 1000000007;
-    public int numOfWays(int[] nums) {
-        List<Integer> list = new ArrayList<>();
-        for (int num : nums) {
-            list.add(num);
+    long mod = (long) 1e9 + 7;
+    long nCr[][];
+
+    public void computeCombinations(int m) {
+        // Pascal Triangle table
+        nCr = new long[m][m];
+
+        for (int i = 0; i < m; i++){
+            nCr[i][0] = 1;
+            nCr[0][i] = 1;
+            nCr[i][i] = 1;
         }
-        return countWays(list) - 1;
-    }
-    private int countWays(List<Integer> nums) {
-        if (nums.size() <= 2) {
-            return 1;
-        }
-        
-        List<Integer> left = new ArrayList<>();
-        List<Integer> right = new ArrayList<>();
-        int root = nums.get(0);
-        
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums.get(i) < root) {
-                left.add(nums.get(i));
-            } else {
-                right.add(nums.get(i));
+
+        for (int n = 2; n < m; n++) {
+            for (int r = 1; r < n; r++) {
+                nCr[n][r] = (nCr[n - 1][r] + nCr[n - 1][r - 1]) % mod;
             }
         }
-        
-        long leftCount = countWays(left);
-        long rightCount = countWays(right);
-        
-        return (int) ((comb(nums.size() - 1, left.size()) % MOD) * (leftCount % MOD) % MOD * (rightCount % MOD) % MOD);
     }
-    
-    private long comb(int n, int k) {
-        long[][] dp = new long[n + 1][k + 1];
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = 1;
-            for (int j = 1; j <= Math.min(i, k); j++) {
-                dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % MOD;
+
+    public long numOfWaysUtil(ArrayList<Integer> nodes) {
+        int n = nodes.size();
+        if (n == 0 || n == 1 || n == 2)
+        return 1;
+
+        int root = nodes.get(0);
+
+        ArrayList<Integer> leftST = new ArrayList<>();
+        ArrayList<Integer> rightST = new ArrayList<>();
+
+        for (int i = 1; i < n; i++) {
+            int node = nodes.get(i);
+            if (node > root){
+                rightST.add(node);
+            }
+            else {
+                leftST.add(node);
             }
         }
-        return dp[n][k];
+
+        long numOfLeftSTRelativeCombinations = numOfWaysUtil(leftST);
+        long numOfRightSTRelativeCombinations = numOfWaysUtil(rightST);
+
+        long numOfLeftRightSTRelativeCombinations =
+        (long) (numOfLeftSTRelativeCombinations * numOfRightSTRelativeCombinations) % mod;
+
+
+        int numOfNodesInLeftST = leftST.size();
+        int numOfNodesInRightST = rightST.size();
+
+        int totPlacesToBeChosen = n - 1;
+
+        return (int) (((long)
+        nCr[totPlacesToBeChosen][numOfNodesInLeftST] * numOfLeftRightSTRelativeCombinations) % mod);
+    }
+
+    public int numOfWays(int[] nums) { 
+        int n = nums.length;
+        computeCombinations(n);
+
+        ArrayList<Integer> numsVarList = new ArrayList<>(n);
+        for(int i = 0; i < n; i++)
+        numsVarList.add(nums[i]);
+
+        return (int) numOfWaysUtil(numsVarList) - 1;
+        
     }
 }
